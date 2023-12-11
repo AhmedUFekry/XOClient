e79f9942715ae58b13a8cbfc0aef61414e3e74dd
rwanelmtary <153429078+rwanelmtary@users.noreply.github.com>
2023-12-11 18:37:35 +0200

    startscreen

--- src/xoclient/StartScreenController.java
+++ src/xoclient/StartScreenController.java
@@ -7,18 +7,26 @@ package xoclient;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
- * FXML Controller class
  *
  * @author Rwan el matry
  */
 public class StartScreenController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
