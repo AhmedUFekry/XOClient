03fc4803c07592c5a8d96a6464706e4e2000b022
Ahmed Fekry <ahmedfekry792@gmail.com>
2023-12-12 03:20:33 +0200

    creating profile screen

--- src/xoclient/XOClient.java
+++ src/xoclient/XOClient.java
@@ -19,7 +19,7 @@ public class XOClient extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = new ProfileScreenBase();
         
         Scene scene = new Scene(root);
         
