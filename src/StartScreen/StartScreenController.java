7bb485f41a73757be645fa6a5c90ec2826c91233
rwanelmtary <153429078+rwanelmtary@users.noreply.github.com>
2023-12-14 03:28:53 +0200

    switch to navigate with controller

---
+++ src/StartScreen/StartScreenController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StartScreen;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import xoclient.Navigate;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rwan el matry
+ */
+public class StartScreenController implements Initializable {
+
+    @FXML
+    private Button singleButton;
+    @FXML
+    private Button dualButton;
+    @FXML
+    private Button onlButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void goToChooseLevelScreen(ActionEvent event) throws IOException {
+           FXMLLoader loader = new FXMLLoader (getClass ().getResource ("/ChooseLevelScreen/ChooseLevelScreen.fxml")) ;
+Parent root = loader.load();
+Navigate.navigateTo(root, event);
+    }
+
+    @FXML
+    private void goToEntrePlayerName(ActionEvent event) throws IOException {
+          FXMLLoader loader = new FXMLLoader (getClass ().getResource ("/EntrePlayerNamesScreen/EntrePlayerNamesScreen.fxml")) ;
+Parent root = loader.load();
+Navigate.navigateTo(root, event);
+    }
+
+    @FXML
+    private void goToLogIn(ActionEvent event) {
+    }
+    
+}
