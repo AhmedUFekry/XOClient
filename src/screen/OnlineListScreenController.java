b21e33a30b97eb872409477a52187e7ee7fb2a94
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2023-12-14 04:26:20 +0200

    feat: online List screen

--- src/screen/OnlineListScreenController.java
+++ src/screen/OnlineListScreenController.java
@@ -3,31 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package xoclient.Screens;
+package screen;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.HPos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.util.Callback;
-import static sun.plugin.javascript.navig.JSType.Image;
 
 /**
  * FXML Controller class
  *
- * @author DELL
+ * @author Geforce
  */
 public class OnlineListScreenController implements Initializable {
 
@@ -38,7 +27,7 @@ public class OnlineListScreenController implements Initializable {
     @FXML
     private Button singlrModeBtn;
     @FXML
-    private ListView<String> listView;
+    private ListView<?> listView;
 
     /**
      * Initializes the controller class.
@@ -46,15 +35,6 @@ public class OnlineListScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
-        listView.setItems(items);
-        listView.setCellFactory(new Callback<ListView<String>, ListCell<String>>(){
-            @Override
-            public ListCell<String> call(ListView<String> param) {
-             return new  CustomListCell();
-            }
-            
-        });       
     }    
 
     @FXML
