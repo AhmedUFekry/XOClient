c62f9f5448ab4da773d47e9d6a9aedf3c6e8fdb0
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2023-12-14 11:23:48 +0200

    feat : RecordList Screen Added

--- src/xoclient/Screens/RecordsScreenController.java
+++ src/xoclient/Screens/RecordsScreenController.java
@@ -7,7 +7,14 @@ package xoclient.Screens;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -16,12 +23,26 @@ import javafx.fxml.Initializable;
  */
 public class RecordsScreenController implements Initializable {
 
+    @FXML
+    private Button backBtn;
+    @FXML
+    private ListView<String> recordListView;
+    private ObservableList<String> items;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+             items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
+             recordListView.setItems(items);
+             recordListView.setCellFactory(new Callback<ListView<String>, ListCell<String>>(){
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+             return new  CustomListCell();
+            }
+            
+        });  
     }    
     
 }
--- src/xoclient/XOClient.java
+++ src/xoclient/XOClient.java
@@ -20,7 +20,7 @@ public class XOClient extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/xoclient/ScreensOnlineListScreenController.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/xoclient/Screens/OnlineListScreenController.fxml"));
 
         Scene scene = new Scene(root);
 
