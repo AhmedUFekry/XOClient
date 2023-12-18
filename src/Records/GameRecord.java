bc0ffd8e09d72afc68ccfcf7071aaa00db0e565d
Ahmed Fekry <ahmedfekry792@gmail.com>
2023-12-18 02:19:19 +0200

    Creating Game Record Class which have the record details

--- src/Records/GameRecord.java
+++ src/Records/GameRecord.java
@@ -10,5 +10,66 @@ package Records;
  * @author Ahmed Fekry
  */
 public class GameRecord {
-    
+
+    private int[] gameMoves;
+    private String firstPlayerName;
+    private String secondPlayerName;
+    private String gameResult;
+
+    /**
+     * @return the gameMoves
+     */
+    public int[] getGameMoves() {
+        return gameMoves;
+    }
+
+    /**
+     * @param gameMoves the gameMoves to set
+     */
+    public void setGameMoves(int[] gameMoves) {
+        this.gameMoves = gameMoves;
+    }
+
+    /**
+     * @return the firstPlayerName
+     */
+    public String getFirstPlayerName() {
+        return firstPlayerName;
+    }
+
+    /**
+     * @param firstPlayerName the firstPlayerName to set
+     */
+    public void setFirstPlayerName(String firstPlayerName) {
+        this.firstPlayerName = firstPlayerName;
+    }
+
+    /**
+     * @return the secondPlayerName
+     */
+    public String getSecondPlayerName() {
+        return secondPlayerName;
+    }
+
+    /**
+     * @param secondPlayerName the secondPlayerName to set
+     */
+    public void setSecondPlayerName(String secondPlayerName) {
+        this.secondPlayerName = secondPlayerName;
+    }
+
+    /**
+     * @return the gameResult
+     */
+    public String getGameResult() {
+        return gameResult;
+    }
+
+    /**
+     * @param gameResult the gameResult to set
+     */
+    public void setGameResult(String gameResult) {
+        this.gameResult = gameResult;
+    }
+
 }
