bc5f1f0387e6c9734d7303bc84b5c96519dfb9ba
Ahmed Fekry <ahmedfekry792@gmail.com>
2023-12-27 19:01:41 +0200

    Fixing bug in offline game

---
+++ src/DTO/DataOperation.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.util.List;
+
+/**
+ *
+ * @author DELL
+ */
+public class DataOperation {
+    private String operation;
+    private List<DTOPlayerData> players;
+
+    public DataOperation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+    public void setPlayers(List<DTOPlayerData> players) {
+        this.players = players;
+    }
+    public String getOperation() {
+        return operation;
+    }
+    public List<DTOPlayerData> getPlayers() {
+        return players;
+    }
+    public DataOperation(String operation, List<DTOPlayerData> players) {
+        this.operation = operation;
+        this.players = players;
+    }
+}
---
+++ src/NetworkManager/NetworkManager.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NetworkManager;
+
+import ClientServer.Client;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author DELL
+ */
+public class NetworkManager {
+    private static Client client;
+    private static InetAddress ipServer;
+    // Private constructor to prevent instantiation
+    private NetworkManager() {}
+    public static void setIpServer(String host) throws UnknownHostException{
+            NetworkManager.ipServer = InetAddress.getByName(host);
+            System.out.println("valid host " + host);
+        
+    }
+    public static synchronized Client getClient() {
+        if (client == null || !client.isAlive()) {
+            if (ipServer == null) {
+                throw new IllegalStateException("Server IP address not set. Call setServerIP() before getClient().");
+            }
+            client = new Client(ipServer);
+            client.start();
+        }
+        return client;
+    }
+    public static synchronized Boolean isClientAlive(){
+        return client.isAlive();
+    }
+}
